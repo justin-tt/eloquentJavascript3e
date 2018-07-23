dd6ee08d4fdc9097c30acfca1b52a41685132a06
justin <radium.laboratories@gmail.com>
2018-07-24 00:36:38 +0800

    5-2 your own loop

---
+++ 5-2.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const loop = function loop(startVal, predicate, update, body) {
+  let val = startVal;
+  while (predicate(val)) {
+    body(val);
+    val = update(val);
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
