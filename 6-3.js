7d0ee6eb8cea02612bcd2856b460d8e810cf39b9
justin <radium.laboratories@gmail.com>
2018-07-25 18:55:32 +0800

    6-3 iterable groups

---
+++ 6-3.js
@@ -0,0 +1,67 @@
+const assert = require('assert');
+
+class Group {
+
+  constructor() {
+    this._elements = [];
+  }
+
+  static from(iterable) {
+    const group = new Group();
+    for (let item of iterable) {
+      group.add(item);
+    }
+    return group;
+  }
+
+  has(item) {
+    return (this._elements.indexOf(item) !== -1);
+  }
+
+  add(item) {
+    if (this._elements.indexOf(item) === -1) {
+      this._elements.push(item);
+    }
+  }
+
+  delete(item) {
+    const itemIndex = this._elements.indexOf(item);
+    if (itemIndex !== -1) {
+      return this._elements.splice(itemIndex, 1)
+    }
+  }
+
+}
+
+class GroupIterator {
+  constructor(group) {
+    this.group = group;
+    this.generatorIndex = 0;
+  }
+
+  next() {
+    // make it an iterable
+    if (this.generatorIndex == this.group._elements.length) return {done: true};
+
+    let value = this.group._elements[this.generatorIndex];
+    this.generatorIndex++;
+    return {value, done: false};
+  }
+}
+
+Group.prototype[Symbol.iterator] = function() {
+  return new GroupIterator(this);
+};
+
+let group = Group.from([10, 20]);
+assert.equal(group.has(10), true);
+assert.equal(group.has(30), false);
+
+group.add(10);
+group.delete(10);
+assert.equal(group.has(10), false);
+
+for (let value of Group.from(["a", "b", "c", "d"])) {
+  console.log(value);
+}
+
